1d0855e809d447cc76888b6cb4b5d5609440c311
promecarus <haikalslipi@gmail.com>
2023-08-12 10:00:28 +0700

    progress

--- app/src/main/java/com/promecarus/affirmations/MainActivity.kt
+++ app/src/main/java/com/promecarus/affirmations/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.promecarus.affirmations
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.promecarus.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.textView).text =
+            Datasource().loadAffirmations().size.toString()
     }
 }
